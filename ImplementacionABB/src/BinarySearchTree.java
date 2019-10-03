e7e077f2bbae4f05aa12fb4a001df02372c8510d
Estudiante <Estudiante@BE401P01S125ING.unal.loc>
2019-10-03 10:39:13 -0500

    BinarySearchTree

--- ImplementacionABB/src/BinarySearchTree.java
+++ ImplementacionABB/src/BinarySearchTree.java
@@ -1,3 +1,6 @@
+
+import com.sun.org.apache.xerces.internal.util.FeatureState;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,7 +12,32 @@
  * @author Estudiante
  */
 public class BinarySearchTree<T extends Comparable<? super T>>{
-    public BinarySearchTree(){
+    private static class BinaryNode<T>{
+        T element;
+        BinaryNode<T> left;
+        BinaryNode<T> right;
+    }
+    private BinaryNode<T> root;
+    
+    public T findMin(){
+        if (isEmpty())
+            throw new UnderflowException();
+        return findMin(root).element;
+    }
+    
+    private BinaryNode<T> findMin(BinaryNode<T> t){
+        if(t==null)
+            return null;
+        else if(t.left==null)
+            return t;
+        return findMin(t.left);
+    }
+    
+    private int height(BinaryNode<T> t){
+        if (t==null) 
+            return -1;
+        else
+            return 1+Math.max(height(t.left), height(t.right));
         
     }
     
